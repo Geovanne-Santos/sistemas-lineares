d5ee84fc5f49ffd33bfc01ad5cefb55e6c7d01a0
Geovanne-Santos <geovamelo4431@gmail.com>
2024-05-15 23:38:44 -0300

    feat: complete

--- src/functions/classificarSistema.js
+++ src/functions/classificarSistema.js
@@ -1,16 +1,7 @@
-// Função para classificar o sistema linear
 export function classificarSistema(detPrincipal, detX, detY, detZ) {
   if (detPrincipal !== 0) {
-    if (detX === 0 && detY === 0 && detZ === 0) {
-      return "O sistema é possível e determinado (solução única).";
-    } else {
-      return "O sistema é possível e indeterminado (infinitas soluções).";
-    }
+    return (detX === 0 && detY === 0 && detZ === 0) ? "O sistema é possível e indeterminado." : "O sistema é possível e determinado.";
   } else {
-    if (detX === 0 && detY === 0 && detZ === 0) {
-      return "O sistema é impossível (sem solução).";
-    } else {
-      return "O sistema é impossível (sem solução consistente).";
-    }
+    return "O sistema é impossível.";
   }
 }
